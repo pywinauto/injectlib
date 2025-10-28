f5f9ba67b2f9c2d20aba6127dc9f9ed265c7e6df
Makarov-AA <43129398+Makarov-AA@users.noreply.github.com>
2025-10-28 08:57:59 +0300

    Return binary files
    Signed-off-by: Makarov-AA <43129398+Makarov-AA@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,46 +1,6 @@
 """Install and build injected distributions"""
 
-import os
-import shutil
-import subprocess
 from setuptools import setup, find_packages
-import sys
-
-
-x86_cmake_arch_name, x64_cmake_arch_name = 'Win32', 'x64'
-x86_package_arch_name, x64_package_arch_name = 'x86', 'x64'
-
-is_64bit = sys.maxsize > 2**32
-if is_64bit:
-    arch_names_map = { x64_cmake_arch_name: x64_package_arch_name }
-else:
-    arch_names_map = { x86_cmake_arch_name: x86_package_arch_name }
-
-build_dirname = 'build_'
-build_dll_dirs = ['./backends/dotnet/', './backends/hook/', './backends/qt/']
-package_dll_dirs = ['./src/injected/libs/dotnet/', './src/injected/libs/hook/', './src/injected/libs/qt/']
-cmake_dirs = build_dll_dirs
-
-# cmake build for DLLs
-for arch in arch_names_map:
-    for cmake_dir in cmake_dirs:
-        build_dir = cmake_dir + build_dirname + arch
-
-        os.makedirs(build_dir, exist_ok=True)
-
-        subprocess.check_call(['cmake', '-B ' + build_dir, '-S ' + cmake_dir, '-A ' + arch])
-        subprocess.check_call(['cmake', '--build', build_dir, '--config', 'Release'])
-
-# copy DLLs to the package
-for arch in arch_names_map:
-    for build_dll_dir, package_dll_dir in zip(build_dll_dirs, package_dll_dirs):
-        for root, _, files in os.walk(build_dll_dir + build_dirname + arch):
-            for file in files:
-                if file.endswith('.dll'):
-                    src_file_path = os.path.join(root, file)
-                    dest_file_path = os.path.join(package_dll_dir + arch_names_map.get(arch), file)
-                    os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)
-                    shutil.copy(src_file_path, dest_file_path)
 
 setup(name='injected',
       version='0.0.1',
